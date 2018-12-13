19fb7bf5f840e71af2460ebe34522bf42df65f7e
vrealchris <36998843+vrealchris@users.noreply.github.com>
2018-12-13 14:57:11 -0800

    Better construction but still wrong answer

--- 06-1.py
+++ 06-1.py
@@ -1,53 +1,61 @@
 """
-Returns the total area of the coordinate with the most spaces around it that are closest to only it
+break up a grid into cells by figuring out the closest point to the cell using Manhattan distance calculation
+Find the area of the largest non-infinite cell
 
-Lookups: wtf is Manhattan Distance?
+Lookups: Manhattan Distance, Voronoi Diagram, Fortune's Algorithm, python's zip function, anonymous functions
 
-Comments: Try 1: build a list of possible coordinates and use that to make a dictionary with [coordinate]: closest point
-for each grid coord, then I can sum up the coordinate with the most matching closest point
+Comments: The brute force method was getting tedious with bug fixes so I started researching better solutions. The Voronoi Diagramming you pointed out had a link to Fortune's Algorithm, which sweeps a line across the grid to limit the data space and backfills solved cells. I dont think I'm up for solving parabolas but I think I can use the concept to at least make solving the puzzle more scalable.
+
+
 """
-my_input = [[1, 1],
-            [1, 6],
-            [8, 3],
-            [3, 4],
-            [5, 5],
-            [8, 9]]
+TESTING = True
+my_input = []
+if TESTING:
+        my_input = [[1,1],
+                    [1,6],
+                    [8,3],
+                    [3,4],
+                    [5,5],
+                    [8,9]]
 
-def compare_cells(cell1, cell2):
-    return abs(cell1[0] - cell2[0]) + abs(cell1[1] - cell2[1])
+if not TESTING:
+    with open('06input.txt') as f:
+        for line in f.readlines():
+            new_line = line.split(',')
+            my_input.append([int(new_line[0]), int(new_line[1])])
 
-max_width = 0
-max_height = 0
+my_input = sorted(my_input)
+scores = {}
+for point in my_input:
+    scores[str(point)] = 0
+x_max = max(min(zip(*my_input)))
+y_max = max(max(zip(*my_input)))
+manhattan = lambda x, y: abs(x[0] - y[0]) + abs(x[1] - y[1])
 
-for i in my_input:
-    if i[0] > max_height:
-        max_height = i[0]
-    if i[1] > max_width:
-        max_width = i[1]
+def closest_point_check(cell, points):
+    #returns a list of coordinates (as strings) which are closest to the cell. 1 means it is completely owned by 1 point
+    closest_distance = 10000
+    measurements = {}
+    for point in points:
+        dist = manhattan(cell, point)
+        if dist < closest_distance:
+            closest_distance = dist
+            measurements[str(point)] = dist
+    closest_list = []
+    for point in measurements:
+        if measurements[point] == closest_distance:
+            closest_list.append(point)
 
-all_cells = []
-for x in range(0, max_height):
-    for y in range(0, max_width):
-        all_cells.append([x,y])
+    return closest_list
 
-calculated_cells = {}
+for x in range(x_max):
+    for y in range(y_max):
+        closest = closest_point_check([x,y], my_input)
+        if len(closest) == 1:
+            scores[closest[0]] += 1
+            if x == 0 or x == x_max+1 or y == 0 or y == y_max+1:
+                scores[closest[0]] += 10000000
 
-for cell1 in all_cells:  # Needs work, not calculating right
-    min_distance = 100000
-    cell_id = 0
-    for cell2 in my_input:
-        cell_id += 1
-        distance = compare_cells(cell1, cell2)
-        if distance < min_distance:
-            min_distance = distance
-            calculated_cells[str(cell1)] = cell_id
-        elif distance == min_distance:
-            calculated_cells[str(cell1)] = None
-            break
-print calculated_cells
-for i in range(len(my_input)):
-    total = 0
-    for thing in calculated_cells.values():
-        if thing == i:
-            total += 1
-    print i, total
+for score in scores:
+    if scores[score] < 10000:
+        print scores[score]
